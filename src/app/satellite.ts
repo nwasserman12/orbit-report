05cdbe9a8233ea6d1acb0a1db8f8219e551f7ca4
nwasserman12 <nickyann0823@aol.com>
2021-04-06 21:25:03 -0500

    half of the table

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -1,9 +1,17 @@
 export class Satellite {
+    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
+        this.name = name; 
+        this.type = type; 
+        this.launchDate = launchDate; 
+        this.orbitType = orbitType; 
+        this.operational = operational; 
+    }
+
     name: string;
     orbitType: string;
     type: string;
     operational: boolean;
     launchDate: string;
 
-    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {}
+     
 }
